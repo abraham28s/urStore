f3ffff8a9643ed839ce7a0053f01381c14a07e96
abraham28s <ab.soto@hotmail.com>
2017-02-13 17:13:31 -0600

    start

---
+++ urStore/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Colors.swift
+//  urStore
+//
+//  Created by Abraham Soto on 13/02/17.
+//  Copyright © 2017 Abraham. All rights reserved.
+//
+
+import Foundation
+
+
+struct GlobalConstants {
+    
+    //En este archivo de deben colocar las variables globales
+    //Se considerara como variable global aquella que se use exactamente la misma
+    //Mas de 3 veces, Se debe colocar un comentario
+    //Con su funcionalidad y/o razon de ser.
+    
+    //Colores(Empleados para el diseño de todo el proyecto)
+    //Son los mismos colores que la paleta personalizada.
+}
